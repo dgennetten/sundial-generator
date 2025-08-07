a99a0d3b922d61e6e237d6913cf9ddb8f0fb4485
Douglas Gennetten <douglas@gennetten.com>
2025-08-07 12:39:42 -0600

    update 'new features'

--- src/App.tsx
+++ src/App.tsx
@@ -402,6 +402,7 @@ const App: React.FC = () => {
                fontSize: '14px',
                fontWeight: '500'
              }}>
+             <p><strong>New Feature:</strong> Map now has searchbox.</p>
              <p><strong>New Feature:</strong> Added Cancer and Capricorn options for Inclined Dials.</p>
              <p><strong>New Feature:</strong> Gnomon height optionally printed on dial.</p>
              <p><strong>New Feature:</strong> Today's date, when plotted, optionally printed on dial.</p>
