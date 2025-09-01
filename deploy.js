ffcfe8286ec5eb97f169efea70bf9e21b89b85ce
Douglas Gennetten <douglas@gennetten.com>
2025-09-01 11:33:05 -0600

    Action Fix (?)

--- deploy.js
+++ deploy.js
@@ -2,6 +2,10 @@ import ftp from 'ftp';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
